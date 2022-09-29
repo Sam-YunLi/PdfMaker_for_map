c32f7cf91502dc088330106c2449b551baef2450
RichaPujara <91420098+RichaPujara@users.noreply.github.com>
2022-09-29 12:02:10 +1000

    Q1 template added

---
+++ Q5.js
@@ -0,0 +1,34 @@
+var dd = {
+  content: [
+    {
+      style: {
+        margin: [10, 15, 10, 15],
+      },
+      table: {
+        style: {
+          width: auto,
+        },
+        headerRows: 1,
+        body: [
+          [
+            {
+              text: "Name and Position",
+              style: "header",
+            },
+            {
+              text: "Company Name and Address",
+              style: "header",
+            },
+            {
+              text: "Email & Phone",
+              style: "header",
+            },
+          ],
+        ],
+      },
+    },
+  ],
+  style: {
+    header: {},
+  },
+};
