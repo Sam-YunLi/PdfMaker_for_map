fe1926d9644cb9a2357dfc55a5d4d3306bd0fb3f
RichaPujara <91420098+RichaPujara@users.noreply.github.com>
2022-09-30 09:58:13 +1000

    Q 1, 5 ,10a updated

--- Q5.js
+++ Q5.js
@@ -1,34 +1,125 @@
 var dd = {
   content: [
     {
-      style: {
-        margin: [10, 15, 10, 15],
-      },
+      fontSize: 13,
+      bold: true,
+      headerRows: 1,
       table: {
-        style: {
-          width: auto,
-        },
-        headerRows: 1,
+        widths: ["*", 20],
         body: [
           [
             {
-              text: "Name and Position",
-              style: "header",
+              fillColor: "black",
+              color: "white",
+              text: "KEY OUTSIDE INFLUENCES LIST",
             },
             {
-              text: "Company Name and Address",
-              style: "header",
-            },
-            {
-              text: "Email & Phone",
-              style: "header",
+              alignment: "RIGHT",
+              text: "Q5",
             },
           ],
         ],
       },
     },
+    {
+      text: " ",
+    },
+    {
+      style: {
+        margin: [10, 15, 10, 15],
+        fontSize: 11,
+        header_table: {
+          bold: true,
+          color: "#FFFFFF",
+          fillColor: "black",
+          alignment: "left",
+        },
+      },
+      layout: {
+        vLineWidth: function (i) {
+          return 0;
+        },
+      },
+      table: {
+        widths: ["*", "*", "*"],
+        headerRows: 1,
+        body: [
+          [
+            {
+              text: "Name and Position",
+              style: "header-table",
+            },
+            {
+              text: "Company Name and Address",
+              style: "header-table",
+            },
+            {
+              text: "Email & Phone",
+              style: "header-table",
+            },
+          ],
+          [
+            "1. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+          [
+            "2. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+          [
+            "3. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+          [
+            "4. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+          [
+            "5. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+          [
+            "6. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+          [
+            "7. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+          [
+            "8. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+          [
+            "9. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+          [
+            "10. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+          [
+            "11. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+          [
+            "12. ID_NAME\nID_TITLE",
+            "ID_CNAME\nID_CADDRESS",
+            "ID_EMAIL\nID_PHONE",
+          ],
+        ],
+      },
+    },
   ],
-  style: {
-    header: {},
-  },
 };
