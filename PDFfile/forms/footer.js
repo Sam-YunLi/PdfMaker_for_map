456d8790362ec19b083f94ed42aa904949c3befe
RichaPujara <91420098+RichaPujara@users.noreply.github.com>
2022-09-30 13:20:20 +1000

    Q11 and footer template added

---
+++ PDFfile/forms/footer.js
@@ -0,0 +1,17 @@
+footer: {
+    columns: [
+      {
+        text: "(please keep a copy of this form)\n 1035572(4) Updated: ID_DATE",
+        alignment: "left",
+        marginLeft: 10,
+        fontSize: 8,
+      },
+      {
+        text: "(over)\nCopyright© 2012 Management Action Programs, Inc.  All rights reserved.",
+        alignment: "right",
+        marginRight: 10,
+        fontSize: 8,
+      },
+    ],
+  }
+  
