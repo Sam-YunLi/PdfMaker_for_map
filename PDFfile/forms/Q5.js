93cb572c0743a7a04f7074853af7d17595e553f2
RichaPujara <91420098+RichaPujara@users.noreply.github.com>
2022-09-30 12:44:36 +1000

    Q 1, 5, 10a header and footer added

--- PDFfile/forms/Q5.js
+++ PDFfile/forms/Q5.js
@@ -1,4 +1,24 @@
+// playground requires you to assign document definition to a variable called dd
+
 var dd = {
+  footer: {
+    columns: [
+      {
+        text: "(please keep a copy of this form)\n ID_FORM_NO Updated: ID_DATE",
+        alignment: "left",
+        marginLeft: 10,
+        fontSize: 8,
+        bold: true,
+      },
+      {
+        text: "(over)\nCopyright© 2012 Management Action Programs, Inc.  All rights reserved.",
+        alignment: "right",
+        marginRight: 10,
+        fontSize: 8,
+        bold: true,
+      },
+    ],
+  },
   content: [
     {
       layout: "noBorders",
@@ -32,11 +52,6 @@ var dd = {
       },
     },
     {
-      fontSize: 11,
-      bold: true,
-      text: "BACK",
-    },
-    {
       fontSize: 13,
       bold: true,
       headerRows: 1,
@@ -47,138 +62,220 @@ var dd = {
             {
               fillColor: "black",
               color: "white",
-              text: "PARTICIPANT'S QUESTIONNAIRE",
+              text: "KEY OUTSIDE INFLUENCES LIST",
             },
             {
-              alignment: "center",
-              text: "Q1",
+              alignment: "RIGHT",
+              text: "Q5",
             },
           ],
         ],
       },
     },
     {
-      style: "table1",
-      table: {
-        body: [
-          [
-            { text: "1.", bold: true },
-            {
-              text: "Please describe your leadership/management strengths as manager.",
-              bold: true,
-            },
-          ],
-          ["", "ID_leadershipSkills"],
-          [
-            { text: "2.", bold: true },
-            {
-              text: "Every leader/manager has opportunities for growth and improvement. What are the management and/or leadership skills in which you could improve?",
-              bold: true,
-            },
-          ],
-          ["", "ID_leadershipSkillsForImprovement"],
-          [
-            { text: "3.", bold: true },
-            {
-              text: "What are your personal career goals in the organization?",
-              bold: true,
-            },
-          ],
-          ["", "ID_personalCareerGoals"],
-          [
-            { text: "4.", bold: true },
-            {
-              text: "What are the one-year measurable goals($, #, %, etc) in your area of responsibility?",
-              bold: true,
-            },
-          ],
-          ["", "ID_oneYearGoal"],
-          [
-            { text: "5.", bold: true },
-            {
-              text: "What are your short term measurable department goals($,#,%, etc.) that can be achieved within next 30 to 45 days?",
-              bold: true,
-            },
-          ],
-          ["", "ID_shortTermGoal"],
-
-          [
-            { text: "6.", bold: true },
-            {
-              text: "What do you believe are the significant challenges you must resolve in order to achieve the above goals?",
-              bold: true,
-            },
-          ],
-          ["", "ID_challenges"],
-          [
-            { text: "7.", bold: true },
-            {
-              text: "What are your main COMMUNICATING challenges(e.g., style, effectiveness, frequency, internal, external,etc.)?",
-              bold: true,
-            },
-          ],
-          ["", "ID_comChallenges"],
-          [
-            { text: "8.", bold: true },
-            {
-              text: "What are your main PLANNING challenges(e.g., no formal planning, unclear goals, projects, etc.)?",
-              bold: true,
-            },
-          ],
-          ["", "ID_planChallenges"],
-          [
-            { text: "9.", bold: true },
-            {
-              text: "What are your main ORGANIZING challenges(e.g., personal organization, availability of supplies, equipment, financial resources, time, organizational structure,etc.)?",
-              bold: true,
-            },
-          ],
-          ["", "ID_orgChallenges"],
-          [
-            { text: "10.", bold: true },
-            {
-              text: "What are your main STAFFING challenges(e.g., recruiting, selecting the right people, training, development, retaining, etc.)?",
-              bold: true,
-            },
-          ],
-          ["", "ID_staffChallenges"],
-          [
-            { text: "11.", bold: true },
-            {
-              text: "What are your main CONTROLLING challenges(e.g., accountability, follow-through, taking corrective actions, cost and inventory control, productivity, quality, etc.)?",
-              bold: true,
-            },
-          ],
-          ["", "ID_contChallenges"],
-          [
-            { text: "12.", bold: true },
-            {
-              text: "What are your main LEADING challenges(e.g., clear vision, motivation, teamwork, initiating action, innovation, commitment to team goals, etc.)?",
-              bold: true,
-            },
-          ],
-          ["", "ID_leadChallenges"],
-          [
-            { text: "13.", bold: true },
-            {
-              text: "What do you expect to gain from this management process? How will you know when those expectations have been met?",
-              bold: true,
-            },
-          ],
-          ["", "ID_expectation"],
-        ],
+      text: " ",
+    },
+    {
+      style: {
+        margin: [10, 15, 10, 15],
+        fontSize: 11,
       },
       layout: {
-        hLineColor: function (rowIndex, node, columnIndex) {
-          return rowIndex % 2 === 0 ? "#000000" : "#FFFFFF";
+        vLineWidth: function (i) {
+          return 0;
         },
-        vLineColor: function (i, node) {
-          return "white";
-        },
+      },
+      table: {
+        widths: ["*", "*", "*"],
+
+        headerRows: 1,
+        body: [
+          [
+            {
+              style: "header_line",
+              text: "Name and Position",
+            },
+            {
+              text: "Company Name and Address",
+              style: "header_line",
+            },
+            {
+              text: "Email & Phone",
+              style: "header_line",
+            },
+          ],
+          [
+            {
+              text: "1. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+          [
+            {
+              text: "2. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+          [
+            {
+              text: "3. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+          [
+            {
+              text: "4. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+          [
+            {
+              text: "5. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+          [
+            {
+              text: "6. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+          [
+            {
+              text: "7. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+          [
+            {
+              text: "8. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+          [
+            {
+              text: "9. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+          [
+            {
+              text: "10. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+          [
+            {
+              text: "11. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+          [
+            {
+              text: "12. ID_NAME\nID_TITLE",
+              style: "listitem",
+            },
+            {
+              text: "ID_CNAME\nID_CADDRESS",
+              style: "listitem",
+            },
+            {
+              text: "ID_EMAIL\nID_PHONE",
+              style: "listitem",
+            },
+          ],
+        ],
       },
     },
   ],
-
   styles: {
     table1: {
       margin: [0, 5, 0, 15],
@@ -190,6 +287,15 @@ var dd = {
     },
     header_left: {
       alignment: "right",
+    },
+    listitem: {
+      lineHeight: 1.2,
+    },
+    header_line: {
+      bold: true,
+      color: "#FFFFFF",
+      fillColor: "black",
+      align: "left",
     },
   },
   images: {
